450351024d5da3acf1fbe66f0bf9d28611edf47b
DreamEcho100 <maze6572198@gmail.com>
2021-11-01 17:18:04 +0200

    - Test.

--- components/UI/V1/CastInfo/index.tsx
+++ components/UI/V1/CastInfo/index.tsx
@@ -6,34 +6,40 @@ import helpers from '@styles/helpers.module.css';
 interface Props {}
 
 const CastInfo = (props: Props) => {
+	const crewListClasses = joinClassNames(
+		classes.crew,
+		helpers.dFlex,
+		helpers.flexWrap
+	);
+
 	return (
 		<section className={classes['cast-info']}>
 			<h4 className={classes['group-title']}>Cast & Crew</h4>
 			<div className={classes.list}>
-				<ul className={classes.crew}>
+				<ul className={crewListClasses}>
 					<li>James</li>
 					<li>George Lucas</li>
 				</ul>
-				<ul className={classes.crew}>
+				<ul className={crewListClasses}>
 					<li>Billy</li>
 					<li>George Lucas</li>
 				</ul>
-				<ul className={classes.crew}>
+				<ul className={crewListClasses}>
 					<li>Liu Kang</li>
 					<li>George Lucas</li>
 				</ul>
-				<ul className={classes.crew}>
+				<ul className={crewListClasses}>
 					<li>Raul</li>
 					<li>George Lucas</li>
 				</ul>
-				<ul className={classes.crew}>
+				<ul className={crewListClasses}>
 					<li>Samantha</li>
 					<li>George Lucas</li>
 				</ul>
 			</div>
 			<h4 className={classes['group-title']}>Director</h4>
 			<div className={classes.list}>
-				<ul className={classes.crew}>
+				<ul className={crewListClasses}>
 					<li>James</li>
 					<li>George Lucas</li>
 				</ul>
