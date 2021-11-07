21ef12bf2ab1ce9046e2f661a4cdc4294db03add
DreamEcho100 <maze6572198@gmail.com>
2021-11-07 19:04:50 +0200

    - Downgrade next version.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 module.exports = (phase) => {
 	const envVars = {
-		experimental: { esmExternals: false }, // There are some modules that are CommonJS module :(
+		// experimental: { esmExternals: false }, // There are some modules that are CommonJS module :(
 		reactStrictMode: true,
 		eslint: {
 			// Warning: This allows production builds to successfully complete even if
